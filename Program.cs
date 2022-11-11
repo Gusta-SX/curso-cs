5c9c454c7a6acfadb5606e816749714bb5c7cdec
Gusta-SX <gugaa24@gmail.com>
2022-11-10 23:14:15 -0300

    linguagem com cs - operação de atribuição

--- Program.cs
+++ Program.cs
@@ -10,19 +10,19 @@ namespace Teste
     // metodo principal
     static void Main(string[] args)
     {
-      // OPERADORES ARITMÉTICOS:
-      // soma => +
-      // subtração => -
-      // multiplicação => *
-      // divisão => /
+      // OPERADORES DE ATRIBUIÇÃO:
+      // utilizamos igual para atribuir um valor
+      // porém, podemos utilizar igual junto a um operador aritmético
+      // x += 5 é o mesmo que x = x + 5
 
-      // seguindo as mesmas regras matematicas, como potenciação primeiro, parenteses e etc
-      // aceita short, int, float e afins
+      int x = 0;
+      x += 5;
+      x -= 1;
+      x *= 10;
+      x /= 2;
 
-      int soma = 22 + 22;
 
-
-      Console.WriteLine(soma);
+      Console.WriteLine(x); // x = 20
     }
   }
 }
